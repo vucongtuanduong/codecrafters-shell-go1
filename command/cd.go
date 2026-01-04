99d4c7f83ae0ccef4d7ae37f39051af5ea6e4b50
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2026-01-04 09:48:49 +0700

    Redirect stdout #jv1

--- command/cd.go
+++ command/cd.go
@@ -8,7 +8,8 @@ import (
 
 func CdCommand(args []string, stdout io.Writer) {
 	if len(args) > 1 {
-		fmt.Errorf("cd command requires only one parameter")
+		fmt.Fprintf(os.Stderr, "cd command requires only one parameter\n")
+		return
 	}
 	directory := args[0]
 	if directory == "~" {
@@ -17,16 +18,20 @@ func CdCommand(args []string, stdout io.Writer) {
 	info, err := os.Stat(directory)
 	if err != nil {
 		if os.IsNotExist(err) {
-			fmt.Errorf("%s: No such file or directory", directory)
+			fmt.Fprintf(os.Stderr, "%s: No such file or directory\n", directory)
+			return
 		}
 		if os.IsPermission(err) {
-			fmt.Errorf("%s: permission denied", directory)
+			fmt.Fprintf(os.Stderr, "%s: permission denied\n", directory)
+			return
 		}
 	}
 	if !info.IsDir() {
-		fmt.Errorf("%s: is not a directory", directory)
+		fmt.Fprintf(os.Stderr, "%s: is not a directory\n", directory)
+		return
 	}
 	if err = os.Chdir(directory); err != nil {
-		fmt.Errorf("%s: failed to change directory", directory)
+		fmt.Fprintf(os.Stderr, "%s: failed to change directory\n", directory)
+		return
 	}
 }
