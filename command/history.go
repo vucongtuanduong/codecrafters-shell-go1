07903195b78957ca4ae80f14fb1e697be203059b
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2026-01-04 17:36:38 +0700

    Listing history #yf5

--- command/history.go
+++ command/history.go
@@ -3,6 +3,7 @@ package command
 import (
 	"fmt"
 	"io"
+	"strconv"
 )
 
 var History []string
@@ -11,7 +12,20 @@ func AppendHistory(cmd string) {
 	History = append(History, cmd)
 }
 func HistoryCommand(args []string, stdout io.Writer) {
-	for i, history := range History {
-		fmt.Printf("%5d  %s\n", i+1, history)
+	if len(args) > 1 {
+		fmt.Println("Too many arguments")
+		return
+	}
+	n, err := strconv.Atoi(args[0])
+	if err != nil {
+		// handle parse error (set default, return, or log)
+		n = 0
+	}
+	minIndex := len(History)
+	if n < minIndex {
+		minIndex = n
+	}
+	for i := 0; i < minIndex; i++ {
+		fmt.Printf("%5d  %s\n", i+1, History[i])
 	}
 }
